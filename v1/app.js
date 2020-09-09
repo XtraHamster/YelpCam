610ac14e95459d3ff8c8972a33899b9cf6676a9e
Martin Krecek <martinkrecek98@seznam.cz>
2020-09-09 15:05:12 +0000

    add new.ejs file, form for add new campgrounds. Update other files, add some buttons for navigation between pages

--- v1/app.js
+++ v1/app.js
@@ -1,21 +1,42 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+var campgrounds = [
+	{name: 'Salmon Creek', image: "https://images.pexels.com/photos/2422265/pexels-photo-2422265.jpeg?auto=compress&cs=tinysrgb&h=350"},
+	{name: 'Iceland Hill', image: "https://images.pexels.com/photos/2419278/pexels-photo-2419278.jpeg?auto=compress&cs=tinysrgb&h=350"}, 
+	{name: 'Mountain Site', image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350"}
+]
 
 app.get('/', function(req, res){
 	res.render('landing');
 });
 
 app.get('/campgrounds', function(req, res){
-	var campgrounds = [
-		{name: 'Salmon Creek', image: "https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e5074407c2a7adc944fc6_340.jpg"},
-		{name: 'Iceland Hill', image: "https://images.pexels.com/photos/2419278/pexels-photo-2419278.jpeg?auto=compress&cs=tinysrgb&h=350"}, 
-		{name: 'Mountain Site', image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350"}
-	]
 	res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+app.post('/campgrounds', function(req, res){
+	// get data from form and add to campgrounds array
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image};
+	campgrounds.push(newCampground);
+	// redirect back to campgrounds page
+	res.redirect('campgrounds');
+});
+
+app.get('/campgrounds/new', function(req, res){
+	res.render('new');
 });
 
 app.listen(3000, function(){
 	console.log('The YelpCamp Server has started!');
 });
+
+
+
+
